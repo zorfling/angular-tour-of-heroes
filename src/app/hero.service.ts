f54dc65421bec003ea710d6e32205f435f911557
Chris Colborne <zorfling@gmail.com>
2018-12-21 18:15:17 +1000

    Add message service - Step 4b

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,16 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message AFTER fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
